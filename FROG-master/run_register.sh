9b4a0ec9a8f821b37d55eef1e705de3c76717a35
MarieFriot <154432198+MarieFriot@users.noreply.github.com>
2026-01-30 15:24:49 +0100

    Update run_register.sh

--- FROG-master/run_register.sh
+++ FROG-master/run_register.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 </chemin/vers/imagesRg> <chemin/vers/images_tr>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 </chemin/vers/imagesRg> <chemin/vers/images_tr> <chemin/vers/dossier/output/>"
     exit 1
 fi
 
 IMAGES_RG=$1
 IMAGES_TR_DIR=$2
+OUTPUT_DIR = $3
 
 # Compter le nombre de masques de référence
 count=$(ls "$IMAGES_RG" | wc -l)
@@ -17,7 +18,7 @@ i=0
 for f in "$IMAGES_TR_DIR"/*.nii.gz; do
 
     # Lancer le script Python qui recale les images cibles sur l'espace commun. Le dossier output est le dossier dans lequel on travail
-    python3 tools/register.py -d ./output -i "$f" -cmin -100 -cmax 500 --orientation "RAS" 
+    python3 tools/register.py -d OUTPUT_DIR "$f" -cmin -100 -cmax 500 --orientation "RAS" 
 
     # Par défaut register.py met le fichier .json de la transformation dans le dossier transforms et la nomme en fonction du nombre de transformation déjà calculé lors du premier recalage
     # Si on a 15 masques de référence, les transformations vont toutes s'écraser dans le dossier transforms sous le nom de 15.json
